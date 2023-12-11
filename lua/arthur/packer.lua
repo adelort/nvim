12c54015473f8b36cf4be73119768d02dc15b976
Arthur <arthur@synthesis.partners>
2023-12-11 10:08:27 +0800

    Properly configure completion

--- lua/arthur/packer.lua
+++ lua/arthur/packer.lua
@@ -51,11 +51,29 @@ return require("packer").startup(function(use)
 
 	use("nvim-treesitter/nvim-treesitter-context")
 
-	use("hrsh7th/cmp-nvim-lsp")
 	use("hrsh7th/cmp-buffer")
 	use("hrsh7th/cmp-path")
 	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/nvim-cmp")
+	use({
+		"hrsh7th/nvim-cmp",
+		after = "nvim-lspconfig",
+	})
+	use({
+		"hrsh7th/cmp-nvim-lsp",
+		after = "nvim-cmp",
+		config = function()
+			require("cmp_nvim_lsp").setup({})
+		end,
+	})
+	use({
+		"hrsh7th/vim-vsnip",
+		after = "nvim-cmp",
+	})
+	use({
+		"hrsh7th/vim-vsnip-integ",
+		after = "vim-vsnip",
+	})
+	use("hrsh7th/cmp-vsnip")
 
 	use("lukas-reineke/indent-blankline.nvim")
 
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -90,14 +90,23 @@ _G.packer_plugins = {
     url = "https://github.com/hrsh7th/cmp-cmdline"
   },
   ["cmp-nvim-lsp"] = {
+    after_files = { "/Users/arthur/.local/share/nvim/site/pack/packer/opt/cmp-nvim-lsp/after/plugin/cmp_nvim_lsp.lua" },
+    config = { "\27LJ\2\n>\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\17cmp_nvim_lsp\frequire\0" },
+    load_after = {},
     loaded = true,
-    path = "/Users/arthur/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    needs_bufread = false,
+    path = "/Users/arthur/.local/share/nvim/site/pack/packer/opt/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-path"] = {
     loaded = true,
     path = "/Users/arthur/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
+  },
+  ["cmp-vsnip"] = {
+    loaded = true,
+    path = "/Users/arthur/.local/share/nvim/site/pack/packer/start/cmp-vsnip",
+    url = "https://github.com/hrsh7th/cmp-vsnip"
   },
   ["ctrlsf.vim"] = {
     loaded = true,
@@ -150,8 +159,11 @@ _G.packer_plugins = {
     url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-cmp"] = {
+    after = { "cmp-nvim-lsp", "vim-vsnip" },
+    load_after = {},
     loaded = true,
-    path = "/Users/arthur/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    needs_bufread = false,
+    path = "/Users/arthur/.local/share/nvim/site/pack/packer/opt/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
@@ -224,6 +236,22 @@ _G.packer_plugins = {
     path = "/Users/arthur/.local/share/nvim/site/pack/packer/start/vim-surround",
     url = "https://github.com/tpope/vim-surround"
   },
+  ["vim-vsnip"] = {
+    after = { "vim-vsnip-integ" },
+    load_after = {},
+    loaded = true,
+    needs_bufread = true,
+    path = "/Users/arthur/.local/share/nvim/site/pack/packer/opt/vim-vsnip",
+    url = "https://github.com/hrsh7th/vim-vsnip"
+  },
+  ["vim-vsnip-integ"] = {
+    after_files = { "/Users/arthur/.local/share/nvim/site/pack/packer/opt/vim-vsnip-integ/after/plugin/vsnip_integ.vim" },
+    load_after = {},
+    loaded = true,
+    needs_bufread = false,
+    path = "/Users/arthur/.local/share/nvim/site/pack/packer/opt/vim-vsnip-integ",
+    url = "https://github.com/hrsh7th/vim-vsnip-integ"
+  },
   ["which-key.nvim"] = {
     loaded = true,
     path = "/Users/arthur/.local/share/nvim/site/pack/packer/start/which-key.nvim",
@@ -232,6 +260,18 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Load plugins in order defined by `after`
+time([[Sequenced loading]], true)
+vim.cmd [[ packadd nvim-lspconfig ]]
+vim.cmd [[ packadd nvim-cmp ]]
+vim.cmd [[ packadd cmp-nvim-lsp ]]
+
+-- Config for: cmp-nvim-lsp
+try_loadstring("\27LJ\2\n>\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\17cmp_nvim_lsp\frequire\0", "config", "cmp-nvim-lsp")
+
+vim.cmd [[ packadd vim-vsnip ]]
+vim.cmd [[ packadd vim-vsnip-integ ]]
+time([[Sequenced loading]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
