d6cacacc2d9586ea73f3b185c09b50bee53483fc
Arthur <arthur@synthesis.partners>
2024-02-29 19:52:18 +0800

    Add a keymap to copy to system clipboard

--- lua/arthur/remap.lua
+++ lua/arthur/remap.lua
@@ -11,6 +11,8 @@ vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
+vim.keymap.set({ "n", "v" }, "<leader>y", '"*y')
+
 vim.keymap.set("n", "<leader>f", function()
 	vim.lsp.buf.format()
 end, { noremap = true })
