7dfbf437592dfee4e0a7656d030c5840ae65dd57
Arthur <arthur@synthesis.partners>
2024-05-11 16:05:18 +0800

    Add vim-visual-multi

--- lua/arthur/lazy.lua
+++ lua/arthur/lazy.lua
@@ -86,4 +86,6 @@ require("lazy").setup({
 	"windwp/nvim-ts-autotag",
 
 	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
+
+	{ "mg979/vim-visual-multi", branch = "master" },
 })
