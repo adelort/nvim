96a521feb50f15f954da7462de4fd5967cfe047b
Arthur <arthurdelort@hotmail.fr>
2024-12-16 17:43:34 +0100

    Use English in neovim instead of French

--- init.lua
+++ init.lua
@@ -8,3 +8,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
+
+vim.env.LANG = 'en_US.UTF-8'
+vim.env.LC_ALL = 'en_US.UTF-8'
+vim.env.LC_CTYPE = 'en_US.UTF-8'
