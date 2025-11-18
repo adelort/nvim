536f67932b392ef2a387921966e2cffce1b29f0d
Arthur <arthurdelort@hotmail.fr>
2025-11-18 23:00:32 +0100

    Add prisma formatting

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -15,7 +15,7 @@ conform.setup({
 
 vim.keymap.set("n", "<leader>f", function()
   conform.format({
-    lsp_fallback = false,
+    lsp_fallback = true,
     async = false,
     timeout_ms = 500
   })
