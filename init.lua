6a39240be532c0c1525eadfe945fef8343adc2f8
Arthur <arthurdelort@hotmail.fr>
2025-11-27 09:28:13 +0100

    Replace vim-surround with nvim-surround

--- init.lua
+++ init.lua
@@ -538,8 +538,11 @@ require("lazy").setup({
 
   -- Editing Enhancements
   {
-    "tpope/vim-surround",
-    event = { "BufReadPost", "BufNewFile" },
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end
   },
   {
     "numToStr/Comment.nvim",
