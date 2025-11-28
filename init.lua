9cc9e835d49c15804754922f3aeb0cc18c4d8f19
Arthur <arthurdelort@hotmail.fr>
2025-11-28 14:12:28 +0100

    Disable breadcrumbs

--- init.lua
+++ init.lua
@@ -331,15 +331,10 @@ require("lazy").setup({
     config = function()
       require("lspsaga").setup({
         move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-        finder_action_keys = {
-          open = "<CR>",
-        },
-        definition_action_keys = {
-          edit = "<CR>",
-        },
-        lightbulb = {
-          enable = false,
-        },
+        finder_action_keys = { open = "<CR>", },
+        definition_action_keys = { edit = "<CR>", },
+        lightbulb = { enable = false, },
+        symbol_in_winbar = { enable = false }
       })
     end,
   },
